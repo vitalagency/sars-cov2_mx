48e7e2af953ba375109754b7ef23672af276c46e
vitalagency <93239157+vitalagency@users.noreply.github.com>
2024-05-06 21:52:20 -0600

    Ref 2020 - 2024

--- sars-cov2_mx_mutation.R
+++ sars-cov2_mx_mutation.R
@@ -1,4 +1,9 @@
+
+# Mexico Secuencias 2020 - 2024
+
 library(seqinr)
+library(dplyr)
+library(ggplot2)
 
 trad =    c(UUU="F", UUC="F", UUA="L", UUG="L",
             UCU="S", UCC="S", UCA="S", UCG="S",
@@ -24,7 +29,7 @@ ToARN = function(input) {
   return (newARN)
 }
 
-df = data.frame(
+df2020_2021 = data.frame(
   Mutation = character(),
   Codon = character(),
   Amino = character(),
@@ -32,185 +37,50 @@ df = data.frame(
   stringsAsFactors = FALSE
 )
 
-df_mut1 = data.frame(
-  Mutation1 = character(),
-  Codon1 = character(),
-  Amino1 = character(),
-  Gene1 = character(),
+df2021_2022 = data.frame(
+  Mutation = character(),
+  Codon = character(),
+  Amino = character(),
+  Gene = character(),
+  stringsAsFactors = FALSE
+)
+
+df2023_2024 = data.frame(
+  Mutation = character(),
+  Codon = character(),
+  Amino = character(),
+  Gene = character(),
   stringsAsFactors = FALSE
 )
 
 
-df_mut2 = data.frame(
-  Mutation2 = character(),
-  Codon2 = character(),
-  Amino2 = character(),
-  Gene2 = character(),
-  stringsAsFactors = FALSE
-)
-
-df_mut3 = data.frame(
-  Mutation3 = character(),
-  Codon3 = character(),
-  Amino3 = character(),
-  Gene3 = character(),
-  stringsAsFactors = FALSE
-)
-
-df_mut4 = data.frame(
-  Mutation4 = character(),
-  Codon4 = character(),
-  Amino4 = character(),
-  Gene4 = character(),
-  stringsAsFactors = FALSE
-)
-
-df_mut5 = data.frame(
-  Mutation5 = character(),
-  Codon5 = character(),
-  Amino5 = character(),
-  Gene5 = character(),
-  stringsAsFactors = FALSE
-)
-
-
-# Primera secuencia México
-ref_2020 = read.fasta("mx1_sequence")
+ref_2020 = read.fasta("sequence_mexico_primera.txt")
 length(ref_2020)
 
-# Mutaciones México 2 : 5
-ref_2021 = read.fasta("mx2_sequence")
+ref_2021 = read.fasta("ref_2022")
 length(ref_2021)
 
-ref_2022 = read.fasta("mx3_sequence")
+## Secuencias 2022 - 2024
+ref_2022 = read.fasta("ref_2022")
 length(ref_2022)
 
-ref_2023 = read.fasta("mx4_sequence")
+ref_2023 = read.fasta("ref_2023")
+length(ref_2023)
+
+ref_2024 = read.fasta("ref_2024")
 length(ref_2024)
-
-ref_2024 = read.fasta("mx5_sequence")
-length(fMx5)
-
-resultados = lista_ref()
-
-for (year in c(2020, 2021, 2022, 2023, 2024)){
-  ref_actual = get(paste("ref_", year, sep=""))
-}
-
-  for (compyear in c(2020, 2021, 2022, 2023, 2024)){
-    if (compyear >= year) next
-    
-    ref_comp = get(paste("ref_", compyear, sep=""))
-    
-    df_mut1 = data.frame(
-      Mutation1 = character(),
-      Codon1 = character(),
-      Amino1 = character(),
-      Gene1 = character(),
-      stringsAsFactors = FALSE
-    )
-    
-    df_mut2 = data.frame(
-      Mutation2 = character(),
-      Codon2 = character(),
-      Amino2 = character(),
-      Gene2 = character(),
-      stringsAsFactors = FALSE
-    )
-    
-    df_mut3 = data.frame(
-      Mutation3 = character(),
-      Codon3 = character(),
-      Amino3 = character(),
-      Gene3 = character(),
-      stringsAsFactors = FALSE
-    )
-    
-    df_mut4 = data.frame(
-      Mutation4 = character(),
-      Codon4 = character(),
-      Amino4 = character(),
-      Gene4 = character(),
-      stringsAsFactors = FALSE
-    )
-    
-    df_mut5 = data.frame(
-      Mutation5 = character(),
-      Codon5 = character(),
-      Amino5 = character(),
-      Gene5 = character(),
-      stringsAsFactors = FALSE
-    )
-    
-    
-    
-  }
+##
 
 
-cat("Procesando ", as.integer(length(ref_2020)/12), " genomas \n")
-cat("Procesando ", as.integer(length(ref_2021)/12), " genomas \n")
-cat("Procesando ", as.integer(length(ref_2022)/12), " genomas \n")
-cat("Procesando ", as.integer(length(ref_2024)/12), " genomas \n")
-cat("Procesando ", as.integer(length(fMx5)/12), " genomas \n")
+cat("Procesando ", as.integer(length(ref_2020)/12), " genomas de Primera Secuencia 2020 y Primera Secuencia 2021 \n")
 
-nObs1 = 1
-nObs2 = 1
-nObs3 = 1
-nObs4 = 1
-nObs5 = 1
-
-
-
-# mutacion 1 
+nObs = 1
 for (i in seq(1,length(ref_2020),1)){
   if (i==2) next
   anotaciones = attr(ref_2020[[i]], "Annot") 
   atributos = unlist(strsplit(anotaciones,"\\[|\\]|:|=|\\.|\\(")); 
   geneName = atributos[which(atributos=="gene")+1] 
-  genRef = ToARN(ref_2020[[i]])  
-  cat("#",geneName)
-  for (k in seq(i, length(ref_2020), 12)){
-    
-    genfRMx = ToARN( ref_2020[[k]] )  
-    cat(i, k, length(genRef), length(genfRMx), "\n")
-    
-    if (length(genRef) == length(genfRMx)){
-      dif = which(genRef != genfRMx)
-      cat("length",length(dif))
-      print(dif)
-      
-      if (length(dif) > 0){
-        for (x in dif){
-          cat(i,k)
-          muta = paste(genRef[x],"to",genfRMx[x], sep="") 
-          inicioCodon = x - (x-1)%%3 
-          numCodon = as.integer((x-1)/3+1) 
-          codonO1 = paste(genRef[inicioCodon], genRef[inicioCodon+1], genRef[inicioCodon+2],sep="")
-          codonref_2020 = paste(genfRMx[inicioCodon], genfRMx[inicioCodon+1], genfRMx[inicioCodon+2],sep="")
-          codonChange = paste(codonO1,"to",codonfRMx, sep="")
-          aminoChange = paste(trad[codonO1],numCodon,trad[codonfRMx], sep="")
-          cat(i,k,geneName, codonChange, aminoChange)
-          
-          if (!is.na(trad[codonfRMx]) && trad[codonO1]!=trad[codonfRMx]){
-            obs = list(muta,codonChange,aminoChange,geneName)
-            df_mut1[nObs1,] = obs 
-            nObs1 = nObs1+1
-          }
-        }
-      }
-    }else{
-    }
-  }
-}
-
-
-#mutacion 2 
-for (i in seq(1,length(ref_2021),1)){
-  if (i==2) next
-  anotaciones = attr(ref_2021[[i]], "Annot") 
-  atributos = unlist(strsplit(anotaciones,"\\[|\\]|:|=|\\.|\\(")); 
-  geneName = atributos[which(atributos=="gene")+1] 
-  genRef = ToARN( ref_2021[[i]] )  
+  genRef = ToARN( ref_2020[[i]] )  
   cat("#",geneName)
   for (k in seq(i, length(ref_2021), 12)){
     
@@ -220,24 +90,22 @@ for (i in seq(1,length(ref_2021),1)){
     if (length(genRef) == length(genref_2021)){
       dif = which(genRef != genref_2021)
       cat("length",length(dif))
-      print(dif)
       
       if (length(dif) > 0){
         for (x in dif){
-          cat(i,k)
           muta = paste(genRef[x],"to",genref_2021[x], sep="") 
           inicioCodon = x - (x-1)%%3 
           numCodon = as.integer((x-1)/3+1) 
-          codonO2 = paste(genRef[inicioCodon], genRef[inicioCodon+1], genRef[inicioCodon+2],sep="")
+          codonOri = paste(genRef[inicioCodon], genRef[inicioCodon+1], genRef[inicioCodon+2],sep="")
           codonref_2021 = paste(genref_2021[inicioCodon], genref_2021[inicioCodon+1], genref_2021[inicioCodon+2],sep="")
-          codonChange = paste(codonO2,"to",codonref_2021, sep="")
-          aminoChange = paste(trad[codonO2],numCodon,trad[codonref_2021], sep="")
+          codonChange = paste(codonOri,"to",codonref_2021, sep="")
+          aminoChange = paste(trad[codonOri],numCodon,trad[codonref_2021], sep="")
           cat(i,k,geneName, codonChange, aminoChange)
           
-          if (!is.na(trad[codonref_2021]) && trad[codonO2]!=trad[codonref_2021]){
+          if (!is.na(trad[codonref_2021]) && trad[codonOri]!=trad[codonref_2021]){
             obs = list(muta,codonChange,aminoChange,geneName)
-            df_mut2[nObs2,] = obs 
-            nObs2 = nObs2+1
+            df2020_2021[nObs,] = obs 
+            nObs = nObs+1
           }
         }
       }
@@ -246,121 +114,19 @@ for (i in seq(1,length(ref_2021),1)){
   }
 }
 
-
-#mutacion 3 
-for (i in seq(1,length(ref_2022),1)){
-  if (i==2) next
-  anotaciones = attr(ref_2022[[i]], "Annot") 
-  atributos = unlist(strsplit(anotaciones,"\\[|\\]|:|=|\\.|\\(")); 
-  geneName = atributos[which(atributos=="gene")+1] 
-  genRef = ToARN( ref_2022[[i]] )  
-  cat("#",geneName)
-  for (k in seq(i, length(ref_2022), 12)){
-    
-    genref_2022 = ToARN( ref_2022[[k]] )  
-    cat(i, k, length(genRef), length(genref_2022), "\n")
-    
-    if (length(genRef) == length(genref_2022)){
-      dif = which(genRef != genref_2022)
-      cat("length",length(dif))
-      print(dif)
-      
-      if (length(dif) > 0){
-        for (x in dif){
-          cat(i,k)
-          muta = paste(genRef[x],"to",genref_2022[x], sep="") 
-          inicioCodon = x - (x-1)%%3 
-          numCodon = as.integer((x-1)/3+1) 
-          codonO3 = paste(genRef[inicioCodon], genRef[inicioCodon+1], genRef[inicioCodon+2],sep="")
-          codonref_2022 = paste(genref_2022[inicioCodon], genref_2022[inicioCodon+1], genref_2022[inicioCodon+2],sep="")
-          codonChange = paste(codonO3,"to",codonref_2022, sep="")
-          aminoChange = paste(trad[codonO3],numCodon,trad[codonref_2022], sep="")
-          cat(i,k,geneName, codonChange, aminoChange)
-          
-          if (!is.na(trad[codonref_2022]) && trad[codonO3]!=trad[codonref_2022]){
-            obs = list(muta,codonChange,aminoChange,geneName)
-            df_mut3[nObs3,] = obs 
-            nObs3 = nObs3+1
-          }
-        }
-      }
-    }else{
-    }
-  }
-}
-
-
-#mutacion 4
-for (i in seq(1,length(ref_2024),1)){
-  if (i==2) next
-  anotaciones = attr(ref_2024[[i]], "Annot") 
-  atributos = unlist(strsplit(anotaciones,"\\[|\\]|:|=|\\.|\\(")); 
-  geneName = atributos[which(atributos=="gene")+1] 
-  genRef = ToARN( ref_2020[[i]] )  
-  cat("#",geneName)
-  for (k in seq(i, length(ref_2024), 12)){
-    
-    genref_2024 = ToARN( ref_2024[[k]] )  
-    cat(i, k, length(genRef), length(genref_2024), "\n")
-    
-    if (length(genRef) == length(genref_2024)){
-      dif = which(genRef != genref_2024)
-      cat("length",length(dif))
-      print(dif)
-      
-      if (length(dif) > 0){
-        for (x in dif){
-          cat(i,k)
-          muta = paste(genRef[x],"to",genref_2024[x], sep="") 
-          inicioCodon = x - (x-1)%%3 
-          numCodon = as.integer((x-1)/3+1) 
-          codonO4 = paste(genRef[inicioCodon], genRef[inicioCodon+1], genRef[inicioCodon+2],sep="")
-          codonref_2024 = paste(genref_2024[inicioCodon], genref_2024[inicioCodon+1], genref_2024[inicioCodon+2],sep="")
-          codonChange = paste(codonO4,"to",codonref_2024, sep="")
-          aminoChange = paste(trad[codonO4],numCodon,trad[codonref_2024], sep="")
-          cat(i,k,geneName, codonChange, aminoChange)
-          
-          if (!is.na(trad[codonref_2024]) && trad[codonO4]!=trad[codonref_2024]){
-            obs = list(muta,codonChange,aminoChange,geneName)
-            df[nObs4,] = obs 
-            nObs4 = nObs4+1
-          }
-        }
-      }
-    }else{
-    }
-  }
-}
-
-
-head(df)
-nrow(df)
-
-library(ggplot2)
-#
-
-
-# Graficar Mutaciones 1..2..3..4
-
-
-p = ggplot(df)
+p = ggplot(df2020_2021)
 p = p + aes(x=Mutation, fill=Mutation, label=after_stat(count))
-p = p + ggtitle("Mutaciones de sustitución")
+p = p + ggtitle("Mutaciones de sustitución México 2020 - 2021")
 p = p + labs(x="Mutation", y="Frecuencia", fill="Frecuencia")
 p = p + geom_bar(stat = "count")
 p = p + geom_text(stat = "count", vjust=1.5)
-#p = p + facet_grid(~Gene)
+p = p + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 p
 
-
-
-# Graficas:
-
-library(dplyr)
-dfgraph = filter(
+df2020_2021gr = filter(
   summarise(
     select(
-      group_by(df, Amino),
+      group_by(df2020_2021, Amino),
       Mutation:Gene
     ),
     Mutation = first(Mutation),
@@ -371,18 +137,30 @@ dfgraph = filter(
   Cuenta>1
 )
 
-dfgraph = dfgraph[order(-dfgraph$Cuenta), ]
-dfgraph = dfgraph[1:20, ]
-head(dfgraph)
-nrow(dfgraph)
+df2020_2021gr = df2020_2021 %>%
+  filter(!is.na(Amino)) %>%
+  group_by(Amino) %>%
+  summarise(
+    mutation2 = first(Mutation),
+    Codon = first(Codon),
+    Gene = first(Gene),
+    Cuenta = n()
+  ) %>%
+  filter(Cuenta > 1) %>%
+  arrange(desc(Cuenta)) %>%
+  slice(1:20)
 
+head(df2020_2021gr)
+nrow(df2020_2021gr)
 
-
-p2 = ggplot(dfgraph)
-p2 = p2 + aes(x=Amino, y=Cuenta, fill=Amino, label=Cuenta)
-p2 = p2 + ggtitle("Cambio de Aminoácidos")
-p2 = p2 + labs(x="Amino", y="Frecuencia", fill="Frecuencia")
+p2 = ggplot(df2020_2021gr)
+p2 = p2 + aes(x = Amino, y = Cuenta, fill = Amino, label = Cuenta)
+p2 = p2 + ggtitle("Cambio de Aminoácidos México 2020 - 2021")
+p2 = p2 + labs(x = "Amino", y = "Frecuencia", fill = "Frecuencia")
 p2 = p2 + geom_bar(stat = "identity")
-p2 = p2 + geom_text(stat = "identity", vjust=1.5)
-p2 = p2 + facet_grid(~Gene, scales="free", space="free_x")
+p2 = p2 + geom_text(stat = "identity", vjust = 1.5)
+p2 = p2 + facet_wrap(~ Gene, scales = "free", space = "free_x")
+p2 = p2 + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 p2
+
+
