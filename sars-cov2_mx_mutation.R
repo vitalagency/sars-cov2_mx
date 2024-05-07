94dfc8410f7eeb45d5fef1b3f429d3ed96559cc2
vitalagency <93239157+vitalagency@users.noreply.github.com>
2024-05-06 23:25:38 -0600

    plots

--- sars-cov2_mx_mutation.R
+++ sars-cov2_mx_mutation.R
@@ -348,14 +348,15 @@ p2
 ##
 ## 2021 --> 2022 Plot Start
 ##
-p = ggplot(df2021_2022)
-p = p + aes(x=Mutation, fill=Mutation, label=after_stat(count))
-p = p + ggtitle("Mutaciones de sustitución México 2021 - 2022")
-p = p + labs(x="Mutation", y="Frecuencia", fill="Frecuencia")
-p = p + geom_bar(stat = "count")
-p = p + geom_text(stat = "count", vjust=1.5)
-p = p + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-p
+## 
+p2021_2022 = ggplot(df2021_2022)
+pp2021_2022 = pp2021_2022 + aes(x=Mutation, fill=Mutation, label=after_stat(count))
+pp2021_2022 = pp2021_2022 + ggtitle("Mutaciones de sustitución México 2021 - 2022")
+pp2021_2022 = pp2021_2022 + labs(x="Mutation", y="Frecuencia", fill="Frecuencia")
+pp2021_2022 = pp2021_2022 + geom_bar(stat = "count")
+p2021_2022 = p2021_2022 + geom_text(stat = "count", vjust=1.5)
+p2021_2022 = p2021_2022 + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p2021_2022
 
 df2021_2022gr = filter(
   summarise(
@@ -387,12 +388,129 @@ df2021_2022gr = df2021_2022 %>%
 head(df2021_2022gr)
 nrow(df2021_2022gr)
 
-p2 = ggplot(df2021_2022gr)
-p2 = p2 + aes(x = Amino, y = Cuenta, fill = Amino, label = Cuenta)
-p2 = p2 + ggtitle("Cambio de Aminoácidos México 2021 - 2022")
-p2 = p2 + labs(x = "Amino", y = "Frecuencia", fill = "Frecuencia")
-p2 = p2 + geom_bar(stat = "identity")
-p2 = p2 + geom_text(stat = "identity", vjust = 1.5)
-p2 = p2 + facet_wrap(~ Gene, scales = "free", space = "free_x")
-p2 = p2 + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-p2
+p3 = ggplot(df2021_2022gr)
+p3 = p3 + aes(x = Amino, y = Cuenta, fill = Amino, label = Cuenta)
+p3 = p3 + ggtitle("Cambio de Aminoácidos México 2021 - 2022")
+p3 = p3 + labs(x = "Amino", y = "Frecuencia", fill = "Frecuencia")
+p3 = p3 + geom_bar(stat = "identity")
+p3 = p3 + geom_text(stat = "identity", vjust = 1.5)
+p3 = p3 + facet_wrap(~ Gene, scales = "free", space = "free_x")
+p3 = p3 + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p3
+##
+## 2021 --> 2022 Plot END
+##
+## ------------------------------------------------------------------------------------------------
+##
+## 2022 --> 2023 Plot Start
+##
+## 
+p2022_2023 = ggplot(df2022_2023)
+pp2022_2023 = pp2021_2022 + aes(x=Mutation, fill=Mutation, label=after_stat(count))
+pp2022_2023 = pp2021_2022 + ggtitle("Mutaciones de sustitución México 2022 - 2023")
+pp2022_2023 = pp2021_2022 + labs(x="Mutation", y="Frecuencia", fill="Frecuencia")
+pp2022_2023 = pp2021_2022 + geom_bar(stat = "count")
+p2022_2023 = p2021_2022 + geom_text(stat = "count", vjust=1.5)
+p2022_2023 = p2021_2022 + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p2022_2023
+
+df2022_2023gr = filter(
+  summarise(
+    select(
+      group_by(df2022_2023, Amino),
+      Mutation:Gene
+    ),
+    Mutation = first(Mutation),
+    Codon = first(Codon),
+    Gene = first(Gene),
+    Cuenta = n()
+  ),
+  Cuenta>1
+)
+
+df2022_2023gr = df2022_2023 %>%
+  filter(!is.na(Amino)) %>%
+  group_by(Amino) %>%
+  summarise(
+    mutation2 = first(Mutation),
+    Codon = first(Codon),
+    Gene = first(Gene),
+    Cuenta = n()
+  ) %>%
+  filter(Cuenta > 1) %>%
+  arrange(desc(Cuenta)) %>%
+  slice(1:20)
+
+head(df2022_2023gr)
+nrow(df2022_2023gr)
+
+p4 = ggplot(df2022_2023gr)
+p4 = p4 + aes(x = Amino, y = Cuenta, fill = Amino, label = Cuenta)
+p4 = p4 + ggtitle("Cambio de Aminoácidos México 2022 - 2023")
+p4 = p4 + labs(x = "Amino", y = "Frecuencia", fill = "Frecuencia")
+p4 = p4 + geom_bar(stat = "identity")
+p4 = p4 + geom_text(stat = "identity", vjust = 1.5)
+p4 = p4 + facet_wrap(~ Gene, scales = "free", space = "free_x")
+p4 = p4 + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p4
+##
+## 2022 --> 2023 Plot END
+##
+## ------------------------------------------------------------------------------------------------
+##
+## 2023 --> 2024 Plot Start
+##
+## 
+p2023_2024 = ggplot(df2023_2024)
+pp2023_2024 = pp2023_2024 + aes(x=Mutation, fill=Mutation, label=after_stat(count))
+pp2023_2024 = pp2023_2024 + ggtitle("Mutaciones de sustitución México 2023 - 2024")
+pp2023_2024 = pp2023_2024 + labs(x="Mutation", y="Frecuencia", fill="Frecuencia")
+pp2023_2024 = pp2023_2024 + geom_bar(stat = "count")
+p2023_2024 = p2023_2024 + geom_text(stat = "count", vjust=1.5)
+p2023_2024 = p2023_2024 + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p2023_2024
+
+df2023_2024gr = filter(
+  summarise(
+    select(
+      group_by(df2023_2024, Amino),
+      Mutation:Gene
+    ),
+    Mutation = first(Mutation),
+    Codon = first(Codon),
+    Gene = first(Gene),
+    Cuenta = n()
+  ),
+  Cuenta>1
+)
+
+df2023_2024gr = df2023_2024 %>%
+  filter(!is.na(Amino)) %>%
+  group_by(Amino) %>%
+  summarise(
+    mutation2 = first(Mutation),
+    Codon = first(Codon),
+    Gene = first(Gene),
+    Cuenta = n()
+  ) %>%
+  filter(Cuenta > 1) %>%
+  arrange(desc(Cuenta)) %>%
+  slice(1:20)
+
+head(df2023_2024gr)
+nrow(df2023_2024gr)
+
+p5 = ggplot(df2023_2024gr)
+p5 = p5 + aes(x = Amino, y = Cuenta, fill = Amino, label = Cuenta)
+p5 = p5 + ggtitle("Cambio de Aminoácidos México 2023 - 2024")
+p5 = p5 + labs(x = "Amino", y = "Frecuencia", fill = "Frecuencia")
+p5 = p5 + geom_bar(stat = "identity")
+p5 = p5 + geom_text(stat = "identity", vjust = 1.5)
+p5 = p5 + facet_wrap(~ Gene, scales = "free", space = "free_x")
+p5 = p5 + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p5
+## ------------------------------------------------------------------------------------------------
+##
+## 2023 --> 2024 Plot END
+##
+## 
