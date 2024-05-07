1f3d603072d8e5d919f88aba902d40c2c2cbd569
vitalagency <93239157+vitalagency@users.noreply.github.com>
2024-05-06 23:11:03 -0600

    Refs 2021 - 2022 2022 - 2023 2023 - 2024

--- sars-cov2_mx_mutation.R
+++ sars-cov2_mx_mutation.R
@@ -1,9 +1,17 @@
 
 # Mexico Secuencias 2020 - 2024
 
+## ------------------------------------------------------------------------------------------------
+
+## Librerias
+
 library(seqinr)
 library(dplyr)
 library(ggplot2)
+
+## ------------------------------------------------------------------------------------------------
+
+## Traductor
 
 trad =    c(UUU="F", UUC="F", UUA="L", UUG="L",
             UCU="S", UCC="S", UCA="S", UCG="S",
@@ -22,12 +30,20 @@ trad =    c(UUU="F", UUC="F", UUA="L", UUG="L",
             GAU="D", GAC="D", GAA="E", GAG="E",
             GGU="G", GGC="G", GGA="G", GGG="G")
 
+## ------------------------------------------------------------------------------------------------
+
+## To ARN Función
+
 ToARN = function(input) {
   newARN = as.vector(input)
   newARN[which(newARN=="t")] = "u"
   newARN = toupper(newARN)
   return (newARN)
 }
+
+## ------------------------------------------------------------------------------------------------
+
+## Data Frames
 
 df2020_2021 = data.frame(
   Mutation = character(),
@@ -45,6 +61,14 @@ df2021_2022 = data.frame(
   stringsAsFactors = FALSE
 )
 
+df2022_2023 = data.frame(
+  Mutation = character(),
+  Codon = character(),
+  Amino = character(),
+  Gene = character(),
+  stringsAsFactors = FALSE
+)
+
 df2023_2024 = data.frame(
   Mutation = character(),
   Codon = character(),
@@ -53,27 +77,34 @@ df2023_2024 = data.frame(
   stringsAsFactors = FALSE
 )
 
+## ------------------------------------------------------------------------------------------------
+
+## Lectura de Secuencias
 
 ref_2020 = read.fasta("ref_2020.txt")
 length(ref_2020)
 
-ref_2021 = read.fasta("ref_2022.txt")
+ref_2021 = read.fasta("ref_2021.txt")
 length(ref_2021)
 
-ref_2022 = read.fasta("ref_2022")
+ref_2022 = read.fasta("ref_2022.txt")
 length(ref_2022)
 
-ref_2023 = read.fasta("ref_2023")
+ref_2023 = read.fasta("ref_2023.txt")
 length(ref_2023)
 
-ref_2024 = read.fasta("ref_2024")
+ref_2024 = read.fasta("ref_2024.txt")
 length(ref_2024)
-##
 
+
+## ------------------------------------------------------------------------------------------------
+##
+## 2020 --> 2021
+##
 
 cat("Procesando ", as.integer(length(ref_2020)/12), " genomas de Primera Secuencia 2020 y Primera Secuencia 2021 \n")
 
-nObs = 1
+nObs2020_2021 = 1
 for (i in seq(1,length(ref_2020),1)){
   if (i==2) next
   anotaciones = attr(ref_2020[[i]], "Annot") 
@@ -103,8 +134,8 @@ for (i in seq(1,length(ref_2020),1)){
           
           if (!is.na(trad[codonref_2021]) && trad[codonOri]!=trad[codonref_2021]){
             obs = list(muta,codonChange,aminoChange,geneName)
-            df2020_2021[nObs,] = obs 
-            nObs = nObs+1
+            df2020_2021[nObs2020_2021,] = obs 
+            nObs2020_2021 = nObs2020_2021+1
           }
         }
       }
@@ -113,6 +144,154 @@ for (i in seq(1,length(ref_2020),1)){
   }
 }
 
+##
+## 2020 --> 2021 END
+##
+## ------------------------------------------------------------------------------------------------
+##
+##
+## 2021 --> 2022 START
+##
+cat("Procesando ", as.integer(length(ref_2021)/12), " genomas de Primera Secuencia 2021 y Primera Secuencia 2022 \n")
+
+nObs2021_2022 = 1
+for (i in seq(1,length(ref_2021),1)){
+  if (i==2) next
+  anotaciones = attr(ref_2021[[i]], "Annot") 
+  atributos = unlist(strsplit(anotaciones,"\\[|\\]|:|=|\\.|\\(")); 
+  geneName = atributos[which(atributos=="gene")+1] 
+  genRef = ToARN( ref_2021[[i]] )  
+  cat("#",geneName)
+  for (k in seq(i, length(ref_2022), 12)){
+    
+    genref_2022 = ToARN( ref_2022[[k]] )  
+    cat(i, k, length(genRef), length(genref_2022), "\n")
+    
+    if (length(genRef) == length(genref_2022)){
+      dif = which(genRef != genref_2022)
+      cat("length",length(dif))
+      
+      if (length(dif) > 0){
+        for (x in dif){
+          muta = paste(genRef[x],"to",genref_2022[x], sep="") 
+          inicioCodon = x - (x-1)%%3 
+          numCodon = as.integer((x-1)/3+1) 
+          codonOri = paste(genRef[inicioCodon], genRef[inicioCodon+1], genRef[inicioCodon+2],sep="")
+          codonref_2022 = paste(genref_2022[inicioCodon], genref_2022[inicioCodon+1], genref_2022[inicioCodon+2],sep="")
+          codonChange = paste(codonOri,"to",codonref_2022, sep="")
+          aminoChange = paste(trad[codonOri],numCodon,trad[codonref_2022], sep="")
+          cat(i,k,geneName, codonChange, aminoChange)
+          
+          if (!is.na(trad[codonref_2022]) && trad[codonOri]!=trad[codonref_2022]){
+            obs2 = list(muta,codonChange,aminoChange,geneName)
+            df2021_2022[nObs2021_2022,] = obs2
+            nObs2021_2022 = nObs2021_2022+1
+          }
+        }
+      }
+    }else{
+    }
+  }
+}
+##
+## 2021 - 2022 END
+##
+## ------------------------------------------------------------------------------------------------
+##
+## 2022 - 2023 Start
+##
+nObs2022_2023 = 1
+for (i in seq(1,length(ref_2022),1)){
+  if (i==2) next
+  anotaciones = attr(ref_2022[[i]], "Annot") 
+  atributos = unlist(strsplit(anotaciones,"\\[|\\]|:|=|\\.|\\(")); 
+  geneName = atributos[which(atributos=="gene")+1] 
+  genRef = ToARN( ref_2022[[i]] )  
+  cat("#",geneName)
+  for (k in seq(i, length(ref_2023), 12)){
+    
+    genref_2023 = ToARN( ref_2023[[k]] )  
+    cat(i, k, length(genRef), length(genref_2023), "\n")
+    
+    if (length(genRef) == length(genref_2023)){
+      dif = which(genRef != genref_2023)
+      cat("length",length(dif))
+      
+      if (length(dif) > 0){
+        for (x in dif){
+          muta = paste(genRef[x],"to",genref_2023[x], sep="") 
+          inicioCodon = x - (x-1)%%3 
+          numCodon = as.integer((x-1)/3+1) 
+          codonOri = paste(genRef[inicioCodon], genRef[inicioCodon+1], genRef[inicioCodon+2],sep="")
+          codonref_2023 = paste(genref_2023[inicioCodon], genref_2023[inicioCodon+1], genref_2023[inicioCodon+2],sep="")
+          codonChange = paste(codonOri,"to",codonref_2023, sep="")
+          aminoChange = paste(trad[codonOri],numCodon,trad[codonref_2023], sep="")
+          cat(i,k,geneName, codonChange, aminoChange)
+          
+          if (!is.na(trad[codonref_2023]) && trad[codonOri]!=trad[codonref_2023]){
+            obs3 = list(muta,codonChange,aminoChange,geneName)
+            df2022_2023[nObs2022_2023,] = obs3
+            nObs2022_2023 = nObs2022_2023+1
+          }
+        }
+      }
+    }else{
+    }
+  }
+}
+##
+## 2022 - 2023 END
+##
+## ------------------------------------------------------------------------------------------------
+##
+## 2023 - 2024 Start
+##
+nObs2023_2024 = 1
+for (i in seq(1,length(ref_2023),1)){
+  if (i==2) next
+  anotaciones = attr(ref_2023[[i]], "Annot") 
+  atributos = unlist(strsplit(anotaciones,"\\[|\\]|:|=|\\.|\\(")); 
+  geneName = atributos[which(atributos=="gene")+1] 
+  genRef = ToARN( ref_2023[[i]] )  
+  cat("#",geneName)
+  for (k in seq(i, length(ref_2024), 12)){
+    
+    genref_2024 = ToARN( ref_2024[[k]] )  
+    cat(i, k, length(genRef), length(genref_2024), "\n")
+    
+    if (length(genRef) == length(genref_2024)){
+      dif = which(genRef != genref_2024)
+      cat("length",length(dif))
+      
+      if (length(dif) > 0){
+        for (x in dif){
+          muta = paste(genRef[x],"to",genref_2024[x], sep="") 
+          inicioCodon = x - (x-1)%%3 
+          numCodon = as.integer((x-1)/3+1) 
+          codonOri = paste(genRef[inicioCodon], genRef[inicioCodon+1], genRef[inicioCodon+2],sep="")
+          codonref_2024 = paste(genref_2024[inicioCodon], genref_2024[inicioCodon+1], genref_2024[inicioCodon+2],sep="")
+          codonChange = paste(codonOri,"to",codonref_2024, sep="")
+          aminoChange = paste(trad[codonOri],numCodon,trad[codonref_2023], sep="")
+          cat(i,k,geneName, codonChange, aminoChange)
+          
+          if (!is.na(trad[codonref_2024]) && trad[codonOri]!=trad[codonref_2024]){
+            obs4 = list(muta,codonChange,aminoChange,geneName)
+            df2023_2024[nObs2023_2024,] = obs4
+            nObs2023_2024 = nObs2023_2024+1
+          }
+        }
+      }
+    }else{
+    }
+  }
+}
+##
+## 2023 - 2024 END
+##
+## ------------------------------------------------------------------------------------------------
+##
+## 2020 --> 2021 Plot
+##
 p = ggplot(df2020_2021)
 p = p + aes(x=Mutation, fill=Mutation, label=after_stat(count))
 p = p + ggtitle("Mutaciones de sustitución México 2020 - 2021")
@@ -161,5 +340,7 @@ p2 = p2 + geom_text(stat = "identity", vjust = 1.5)
 p2 = p2 + facet_wrap(~ Gene, scales = "free", space = "free_x")
 p2 = p2 + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 p2
-
+##
+## 2020 --> 2021 Plot
+##
 
